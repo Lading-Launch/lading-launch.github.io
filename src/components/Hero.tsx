554f217cdbd1590cdc17a4ed1b4127f51a873902
Owen Dupree <william.dupree@accenture.com>
2025-11-21 14:26:03 -0500

    add webp for hero asset, added nvmrc, moved import statement

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import logo from "@/assets/lading_launch.svg";
+import logo from "@/assets/lading_launch_hero.webp";
 
 const Hero = () => {
   return (
